8789532c131f5d2c9b95dc11b24b10fe6da6dc39
pmartinp <102436420+pmartinp@users.noreply.github.com>
2022-04-25 10:41:06 +0200

    Add files via upload

--- src/ficheros/Main.java
+++ src/ficheros/Main.java
@@ -50,6 +50,7 @@ public class Main {
 					
 				//Ordenar personas
 				case 3:
+				
 					ordenarPersonas();
 					break;
 					
@@ -97,19 +98,19 @@ public class Main {
 		BufferedReader br = null;
 		try {
 			FileReader doc = new FileReader("files/numeros.txt");
-			br = new BufferedReader(doc);//leemos el archivo
+			br = new BufferedReader(doc);//leemos el archivo.
 			String line=br.readLine();
 			
-			//añadimos el primer numero de la lista a nuestras variables máximo y mínimo
+			//añadimos el primer numero de la lista a nuestras variables máximo y mínimo.
 			maximo = Integer.valueOf(line);
 			minimo = Integer.valueOf(line);
-			while(line != null) {//recorremos nuestro documento por lineas
+			while(line != null) {//recorremos nuestro documento por lineas.
 				numero = Integer.valueOf(line);
 				if(numero>maximo) {
-					maximo=numero;//cada vez que haya un número mayor actualizamos nuestro máximo
+					maximo=numero;//cada vez que haya un número mayor actualizamos nuestro máximo.
 				}
 				else if(numero<minimo) {
-					minimo=numero;//cada vez que haya un número menor actualizamos nuestro mínimo
+					minimo=numero;//cada vez que haya un número menor actualizamos nuestro mínimo.
 				}
 				line=br.readLine();
 			}
@@ -133,10 +134,10 @@ public class Main {
 			File libros = new File("files/MEDIA/Libros");
 			File renLibros = new File("files/MEDIA/LECTURA");
 			
-			if(media.renameTo(renMedia) && fotos.renameTo(renFotos) && libros.renameTo(renLibros)) {//renombramos las carpetas y comprobamos que se hayan renombrado con éxito
+			if(media.renameTo(renMedia) && fotos.renameTo(renFotos) && libros.renameTo(renLibros)) {//renombramos las carpetas y comprobamos que se hayan renombrado con éxito.
 				System.out.println("Carpetas renombradas con éxito");
 			}else {
-				System.out.println("Error");
+				System.out.println("Error, ya se han renombrado las carpetas.");
 			}
 		}catch(Exception ex) {
 			ex.printStackTrace();
@@ -144,56 +145,64 @@ public class Main {
 	}
 	
 	public static void ordenarPersonas() {
-		Scanner leer= new Scanner(System.in);
-		BufferedReader br = null;
-		BufferedWriter bw = null;
+		File proceso = new File("files/listado_personas_ordenado.txt");
 		
-		String SEPARATOR=" ";
-		int contador=1;
-		
-		TreeSet<Persona> personas=new TreeSet();
-		
-		System.out.println("Introduzca el nombre del archivo que desea leer.");
-		System.out.println("Sugerencias: 'listado_personas.txt'");
-		String archivo = leer.nextLine();
-		
-		while(!comprobarRuta(archivo)) {//comprobamos que la dirección sea correcta
-			System.out.println("Archivo no encontrado");
-			archivo = leer.nextLine();
-		}
-		
-		try {
-			FileReader doc = new FileReader("files/" + "\\" + archivo);
-			br = new BufferedReader(doc);//leemos el archivo
-			String line=br.readLine();
+		if(proceso.exists()) {
+			System.out.println("Ya existe el archivo listado_personas_ordenado.txt");
+		}else {
+			Scanner leer= new Scanner(System.in);
+			BufferedReader br = null;
+			BufferedWriter bw = null;
 			
-			while(line!=null) {//recorremos nuestro documento por lineas
-				 String [] fields = line.split(SEPARATOR);//separamos los elementos por " "
-				 Persona persona= new Persona(contador, fields[0], fields[1]);
-				 
-				 personas.add(persona);//añadimos personas a nuestro TreeSet (se ordena automaticamente)
-				 contador++;		
-				 line=br.readLine();
+			String SEPARATOR=" ";
+			int contador=1;
+			
+			TreeSet<Persona> personas=new TreeSet();
+			
+			System.out.println("Introduzca el nombre del archivo que desea leer.");
+			System.out.println("Sugerencias: 'listado_personas.txt'");
+			String archivo = leer.nextLine();
+			
+			while(!comprobarRuta(archivo)) {//comprobamos que la dirección sea correcta.
+				System.out.println("Archivo no encontrado");
+				archivo = leer.nextLine();
 			}
-			br.close();
-		}catch(IOException ex){
-			ex.printStackTrace();
-		}
-		
-		try {
-			FileWriter newDoc = new FileWriter("files/listado_personas_ordenado.txt");
-			bw = new BufferedWriter(newDoc);
 			
-			bw.write(personas.toString().replaceAll("[\\[\\]]", "").replace(", ", ""));//escribimos un nuevo documento basado en nuestro array de personas ordenado
-			bw.close();
-		}catch(IOException ex){
-			ex.printStackTrace();
+			try {
+				FileReader doc = new FileReader("files/" + "\\" + archivo);
+				br = new BufferedReader(doc);//leemos el archivo.
+				String line=br.readLine();
+				
+				while(line!=null) {//recorremos nuestro documento por lineas.
+					 String [] fields = line.split(SEPARATOR);//separamos los elementos por " ".
+					 Persona persona= new Persona(contador, fields[0], fields[1]);
+					 
+					 personas.add(persona);//añadimos personas a nuestro TreeSet (se ordena automaticamente).
+					 contador++;		
+					 line=br.readLine();
+				}
+				br.close();
+			}catch(IOException ex){
+				ex.printStackTrace();
+			}
+			
+			try {
+				FileWriter newDoc = new FileWriter("files/listado_personas_ordenado.txt");//creamos un nuevo documento.
+				bw = new BufferedWriter(newDoc);
+				
+				bw.write(personas.toString().replaceAll("[\\[\\]]", "").replace(", ", ""));//escribimos en el nuevo documento los datos de nuestro array de personas ordenado.
+				bw.close();
+			}catch(IOException ex){
+				ex.printStackTrace();
+			}
+			
+			System.out.println("Archivo creado con éxito.");
 		}
 		
 		
 	}
 	
-	public static boolean comprobarRuta(String str) {//método para comprobar que la ruta introducida en la opción 3 del menú sea la correcta
+	public static boolean comprobarRuta(String str) {//método para comprobar que la ruta introducida en la opción 3 del menú sea la correcta.
 		if(str.equals("listado_personas.txt")) {
 			return true;
 		}else {
@@ -209,19 +218,21 @@ public class Main {
 			File Fotos = new File("files/MEDIA/FOTOS");
 			File Libros = new File("files/MEDIA/LECTURA");
 			
-			archivosFotos=Fotos.listFiles();
+			archivosFotos=Fotos.listFiles();//creamos un array con todos los archivos de nuestra carpeta FOTOS.
 			
-			for(File x: archivosFotos) {
-				File renombrar = new File("files/MEDIA/FOTOS/"+x.getName().replace(".jpg", ""));
+			for(File x: archivosFotos) {//recorremos los archivos de nuestra carpeta FOTOS
+				File renombrar = new File("files/MEDIA/FOTOS/"+x.getName().replaceAll("\\..*$", ""));//sustituimos la extensión.
 				x.renameTo(renombrar);
 			}
 			
-			archivosLibros=Libros.listFiles();
+			archivosLibros=Libros.listFiles();//creamos un array con todos los archivos de nuestra carpeta LIBROS.
 			
-			for(File x: archivosLibros) {
-				File renombrar = new File("files/MEDIA/LECTURA/"+x.getName().replace(".txt", ""));
+			for(File x: archivosLibros) {//recorremos los archivos de nuestra carpeta LIBROS.
+				File renombrar = new File("files/MEDIA/LECTURA/"+x.getName().replaceAll("\\..*$", ""));//sustituimos la extensión.
 				x.renameTo(renombrar);
 			}
+			
+			System.out.println("Archivos renombrados con éxito");
 			
 		}catch(Exception ex) {
 			ex.printStackTrace();
@@ -232,38 +243,38 @@ public class Main {
 		Scanner leer = new Scanner(System.in);
 		BufferedReader br;
 		
-		int numLineas=1;
-		int numPalabras=0;
-		int numCaracteres=0;
-		int iterador=0;
+		int numLineas=1;//contador de lineas.
+		int numPalabras=0;//contador de palabras.
+		int numCaracteres=0;//contador de caracteres.
+		int iterador=0;//iterador para recorrer nuestro HashMap.
 		HashMap<String, Integer> repPalabras=new HashMap();
 		
 		File Libros = new File("files/MEDIA/LECTURA");
-		File[]archivosLibros = archivosLibros=Libros.listFiles();
+		File[]archivosLibros = archivosLibros=Libros.listFiles();//creamos un array con los posibles documentos a analizar las estadísticas.
 		
 		System.out.println("Escribe la ruta del archivo del que quieras saber sus estadísticas");
 		System.out.print("Sugerencias: ");
 		
 		for(File x: archivosLibros) {
-			System.out.print("'"+x.getName()+"', ");
+			System.out.print("'"+x.getName()+"', ");//nombre de los archivos.
 		}
 		System.out.println("");
 		String archivo=leer.next();
 		
-		while(!comprobarRutaLibros(archivo, archivosLibros)) {
+		while(!comprobarRutaLibros(archivo, archivosLibros)) {//comprobamos que la ruta introducida sea la correcta.
 			System.out.println("Ruta no encontrada, introduzca nueva ruta");
 			archivo=leer.next();
 		}
 		
 		try {
-			FileReader doc = new FileReader("files/MEDIA/LECTURA/" + "\\" + archivo);
+			FileReader doc = new FileReader("files/MEDIA/LECTURA/" + "\\" + archivo);//leemos el archivo introducido por el usuario.
 			br = new BufferedReader(doc);
 			String line=br.readLine();
 			
 			while(line!=null) {
 				numLineas++;
 				
-				numPalabras+=contarPalabras(line, repPalabras);		
+				numPalabras+=contarPalabras(line, repPalabras);
 				numCaracteres+=contarCaracter(line);
 				line=br.readLine();
 			}
@@ -272,14 +283,14 @@ public class Main {
 			ex.printStackTrace();
 		}
 		
-		Map<String, Integer> sortedMapDesc = sortByComparator(repPalabras, DESC);
+		Map<String, Integer> sortedMapDesc = sortByComparator(repPalabras, DESC);//ordenamos nuestro map de manera descendente por su valor.
 		
 		System.out.println("Número de líneas: "+ numLineas);
 		System.out.println("Número de palabras: "+ numPalabras);
 		System.out.println("Número de carácteres: "+ numCaracteres);
 		System.out.println("Las 10 palabras mas repetidas son: ");
 		
-		for(Map.Entry<String, Integer> entrada : sortedMapDesc.entrySet()) {
+		for(Map.Entry<String, Integer> entrada : sortedMapDesc.entrySet()) {//mostramos los 10 primeros resultados de nuestro map.
 			if(iterador<10) {
 				System.out.println(entrada);
 				iterador++;
@@ -311,7 +322,7 @@ public class Main {
 	            }
 	        });
 
-	        // Maintaining insertion order with the help of LinkedList
+	        // Maintaining insertion order with the help of LinkedList.
 	        Map<String, Integer> sortedMap = new LinkedHashMap<String, Integer>();
 	        for (Entry<String, Integer> entry : list)
 	        {
@@ -323,18 +334,18 @@ public class Main {
 	
 	public static int contarPalabras(String line, HashMap<String, Integer> mapa) {
 		int numPalabras=0;
-		line=line.trim();
-		line=line.toLowerCase();
-		line=line.replaceAll("\\p{Punct}", "");
+		line=line.trim();//eliminamos espacios del principio y final del string.
+		line=line.toLowerCase();//convertimos el string a minúscula para evitar errores a la hora de contar la misma palabra.
+		line=line.replaceAll("\\p{Punct}", "");//eliminamos todo signo de puntuación para evitar errores a la hora de contar palabras.
 		if(!line.matches("")) {
-			String[] palabras=line.split(" +");
+			String[] palabras=line.split(" +");//separamos las palabras por espacios.
 			numPalabras+=palabras.length;
 			for(String x: palabras) {
 				if(mapa.containsKey(x)) {
-					mapa.put(x, (mapa.get(x)+1));
+					mapa.put(x, (mapa.get(x)+1));//si nuestro palabra está en nuestro map sumamos 1 al valor.
 				}
 				else {
-					mapa.put(x, 1);
+					mapa.put(x, 1);//si la palabra no está en nuestro map la añadimos y contamos 1.
 				}
 			}
 		}
@@ -343,7 +354,7 @@ public class Main {
 	
 	public static int contarCaracter(String line) {
 		int numCaracteres=0;
-		line=line.replaceAll(" ", "");
+		line=line.replaceAll(" ", "");//contamos todos los carácteres menos los espacios.
 		numCaracteres+=line.length();
 		
 		return numCaracteres;
